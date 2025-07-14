6c7e823661df4a1b4ba7922db164eda77607f480
jesingson <jesingson@gmail.com>
2025-07-13 22:54:55 -0400

    Updated main.py for Render deployment

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import panel as pn
+from youtube_dashboard import create_interactive_dashboard  # Or whatever function you use
+
+# Call this once to initialize widgets and dashboard
 pn.extension('plotly')
 
-# Import the notebook as a module (requires jupyter, nbconvert, etc.)
-pn.serve("youtube_dashboard.ipynb", 
-         port=8000, 
-         address="0.0.0.0", 
-         allow_websocket_origin=["*"],
-         autoreload=True,
-         show=False)
+# Initial render (you can customize inputs here or use default ones)
+dashboard = create_interactive_dashboard(
+    metric='log_views',
+    catFilters=['Music'],
+    countryFilters=['US', 'CA', 'GB'],
+    bins=50,
+    xAxisMetric='log_likes',
+    xAxisDate='publish_time',
+    periodRollup='Weekly'
+)
+
+dashboard.servable()
